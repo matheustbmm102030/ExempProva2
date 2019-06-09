71f8660e8101f9ea38ca9d4195c72bb4092a8a74
medei <medei@LAPTOP-H8E7BD2M>
2019-06-09 19:20:20 -0300

    3

---
+++ src/Quest3_Comentada/NewClass.java
@@ -0,0 +1,29 @@
+
+package Quest3_Comentada;
+
+public class NewClass {
+    
+    //I- Certa
+    
+    //II-Certa
+    
+    //III-Não pois ta retornando string e a função e do tipo int
+    
+    //IV-não pois os primeiros foram refeitos nas seguintes classes
+    
+    //V- Certa
+    
+    //VI-Certa
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
